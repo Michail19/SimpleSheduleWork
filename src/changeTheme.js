c2c832b5ccf98fc06d8bf5d60304c21f369359b1
Michail19 <Mi.ershov.2017@yandex.ru>
2025-03-20 23:27:29 +0300

    Some cosmetic update

--- src/changeTheme.js
+++ src/changeTheme.js
@@ -1,3 +1,16 @@
 document.querySelector('.header__up-blocks__theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
 });
+
+document.addEventListener('click', (event) => {
+    const button = event.target.closest('.btn_worksheet');
+    const content = document.querySelector('.content');
+    const subtitle_date = document.querySelector('.subtitle__date');
+    const footer = document.querySelector('.footer');
+    if (button && content && subtitle_date && footer) {
+        button.classList.toggle('btn_worksheet_enable');
+        content.classList.toggle('hide_content');
+        subtitle_date.classList.toggle('hide_content')
+        footer.classList.toggle('hide_content')
+    }
+});
