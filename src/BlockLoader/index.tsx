c1aaa7b523b5dcca161b0e84649f7e3ad016515e
Michail19 <Mi.ershov.2017@yandex.ru>
2025-04-27 15:51:50 +0300

    Loader

--- src/BlockLoader/index.tsx
+++ src/BlockLoader/index.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './BlockLoader.css'; // стили отдельно или в глобальных стилях
+
+export function touch_on_load() {
+    const button_hide = document.querySelector('.btn_worksheet');
+    const footer_hide = document.querySelector('.footer');
+
+    if (button_hide && footer_hide && localStorage.getItem('btn_state') !== 'hidden') {
+        button_hide.classList.toggle('btn_worksheet_enable');
+        footer_hide.classList.toggle('hide_content');
+    }
+}
 
 const BlockLoader: React.FC = () => {
     return (
