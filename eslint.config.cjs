4fbcd1f69266da70677d25e9a72fd341f1116132
Michail19 <Mi.ershov.2017@yandex.ru>
2025-04-28 16:40:18 +0300

    upd

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,27 +1,38 @@
-const js = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-const reactPlugin = require('eslint-plugin-react');
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
 
-module.exports = {
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            extends: [
-                js.configs.recommended,
-                tseslint.configs.recommended
-            ],
-            plugins: {
-                react: reactPlugin
-            },
-            rules: {
-                'react/react-in-jsx-scope': 'off', // если используешь новые версии React
-                'react/prop-types': 'off',          // если не используешь PropTypes (с TypeScript они не нужны)
-            },
-            settings: {
-                react: {
-                    version: 'detect',
-                },
+export default [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        plugins: {
+            react: reactPlugin,
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off', // если используешь новые версии React
+            'react/prop-types': 'off',          // если не используешь PropTypes (с TypeScript они не нужны)
+        },
+        settings: {
+            react: {
+                version: 'detect',
             },
         },
-    ],
-};
+    },
+    {
+        files: ['*.ts', '*.tsx'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+        ],
+        rules: {
+            'react/react-in-jsx-scope': 'off', // если используешь новые версии React
+            'react/prop-types': 'off',          // если не используешь PropTypes (с TypeScript они не нужны)
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+];
