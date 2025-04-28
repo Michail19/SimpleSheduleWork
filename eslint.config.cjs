0bfc2f429cb81fb0ccd562082f2b9ac75e0d4601
Michail19 <Mi.ershov.2017@yandex.ru>
2025-04-28 16:38:24 +0300

    upd

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,21 +2,26 @@ const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const reactPlugin = require('eslint-plugin-react');
 
-module.exports = [
-    js.configs.recommended,
-    tseslint.configs.recommended,
-    {
-        plugins: {
-            react: reactPlugin,
-        },
-        rules: {
-            'react/react-in-jsx-scope': 'off', // если используешь новые версии React
-            'react/prop-types': 'off',          // если не используешь PropTypes (с TypeScript они не нужны)
-        },
-        settings: {
-            react: {
-                version: 'detect',
+module.exports = {
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            extends: [
+                js.configs.recommended,
+                tseslint.configs.recommended
+            ],
+            plugins: {
+                react: reactPlugin
+            },
+            rules: {
+                'react/react-in-jsx-scope': 'off', // если используешь новые версии React
+                'react/prop-types': 'off',          // если не используешь PropTypes (с TypeScript они не нужны)
+            },
+            settings: {
+                react: {
+                    version: 'detect',
+                },
             },
         },
-    },
-];
+    ],
+};
