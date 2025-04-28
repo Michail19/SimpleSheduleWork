a15ce582425288364576823b4786033594ecbab7
Michail19 <Mi.ershov.2017@yandex.ru>
2025-04-28 16:33:54 +0300

    cjs jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-    },
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
