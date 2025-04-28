e69756eaca14bd7a756fa4a17daa84871ed1e7eb
Michail19 <Mi.ershov.2017@yandex.ru>
2025-04-28 15:46:02 +0300

    Linter

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        plugins: {
+            react: reactPlugin,
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off', // если используешь новые версии React
+            'react/prop-types': 'off',          // если не используешь PropTypes (с TypeScript они не нужны)
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+];
