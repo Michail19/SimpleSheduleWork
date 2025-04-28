6e1d97d3ab14f0962f412bbfae261bcba34e69df
Michail19 <Mi.ershov.2017@yandex.ru>
2025-04-28 16:50:01 +0300

    upd

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,7 +1,6 @@
 const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const reactPlugin = require('eslint-plugin-react');
-const globals = require('globals');
 
 module.exports = tseslint.config(
     {
@@ -9,8 +8,18 @@ module.exports = tseslint.config(
         ignores: ['**/*.config.js'],
         languageOptions: {
             globals: {
-                ...globals.browser, // Добавляем глобальные переменные браузера
-                ...globals.node,    // Добавляем глобальные переменные Node.js
+                // Вручную определяем необходимые глобальные переменные
+                document: 'readonly',
+                window: 'readonly',
+                localStorage: 'readonly',
+                setTimeout: 'readonly',
+                CustomEvent: 'readonly',
+                // Добавьте другие необходимые глобальные переменные
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
             },
         },
     },
@@ -23,13 +32,9 @@ module.exports = tseslint.config(
         rules: {
             'react/react-in-jsx-scope': 'off',
             'react/prop-types': 'off',
-
-            // Правила для TypeScript
             '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
             '@typescript-eslint/no-explicit-any': 'warn',
             '@typescript-eslint/ban-ts-comment': 'warn',
-
-            // Правила для JavaScript
             'no-undef': 'error',
             'prefer-const': 'warn',
         },
